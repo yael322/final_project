306a601e59c2e595c42bd4776b7bedec724902f4
Yael <yaelili322@gmail.com>
2026-06-13 14:35:21 +0300

    Update db_logic.py

--- db_logic.py
+++ db_logic.py
@@ -1,23 +1,53 @@
 import sqlite3
 
-def create_db():
-    conn = sqlite3.connect("hsr.db")
-    cursor = conn.cursor()
+class Bot:
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS characters (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL UNIQUE,
-        rarity INTEGER,
-        path TEXT,
-        element TEXT,
-        faction TEXT,
-        planet TEXT,
-        description TEXT
-    )
-    """)
+    @staticmethod
+    def create_db():
+        conn = sqlite3.connect("hsr.db")
+        cursor = conn.cursor()
 
-    conn.commit()
-    conn.close()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS characters (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL UNIQUE,
+            rarity INTEGER,
+            path TEXT,
+            element TEXT,
+            faction TEXT,
+            planet TEXT,
+            description TEXT
+        )
+        """)
 
-create_db()
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    def get_character_info(character_name):
+        conn = sqlite3.connect("hsr.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT *
+            FROM characters
+            WHERE LOWER(name) = LOWER(?)
+        """, (character_name,))
+
+        result = cursor.fetchone()
+
+        conn.close()
+
+        if result is None:
+            return "Character not found!"
+
+        return {
+            "id": result[0],
+            "name": result[1],
+            "rarity": result[2],
+            "path": result[3],
+            "element": result[4],
+            "faction": result[5],
+            "planet": result[6],
+            "description": result[7]
+        }

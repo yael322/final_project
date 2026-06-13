a934d1fa637035edd8a2788a6c627af77e78ce78
Yael <yaelili322@gmail.com>
2026-06-13 14:17:01 +0300

    Update db_logic.py

--- db_logic.py
+++ db_logic.py
@@ -1,1 +1,23 @@
+import sqlite3
 
+def create_db():
+    conn = sqlite3.connect("hsr.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS characters (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL UNIQUE,
+        rarity INTEGER,
+        path TEXT,
+        element TEXT,
+        faction TEXT,
+        planet TEXT,
+        description TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+create_db()
